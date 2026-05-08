4327a44875a76b4174c07806b89468e5974af8fc
Annika Egger <annikaegger83@gmail.com>
2026-05-08 12:25:06 +0200

    feat: styling prio buttons, svgs by click

---
+++ scripts/add-task.js
@@ -0,0 +1,34 @@
+let priority = "medium";
+
+function setPriority(prio) {
+  priority = prio;
+  stylePrioBtns();
+  stylePrioSvgs();
+}
+
+function stylePrioBtns() {
+  let activeBtn = document.getElementById(priority + "-prio-btn");
+
+  document.querySelectorAll(".prio-btn").forEach((btn) => {
+    if (btn !== activeBtn) {
+      btn.classList.remove("urgent-active", "medium-active", "low-active");
+    }
+  });
+  activeBtn.classList.toggle(priority + "-active");
+}
+
+function stylePrioSvgs() {
+  let activeSvg = document.getElementById(priority + "-prio-svg");
+
+  document.querySelectorAll(".prio-svg").forEach((svg) => {
+    if (svg !== activeSvg) {
+      svg.querySelectorAll("path").forEach((path) => {
+        path.classList.remove("active-svg");
+      });
+    }
+  });
+
+  activeSvg.querySelectorAll("path").forEach((path) => {
+    path.classList.toggle("active-svg");
+  });
+}
