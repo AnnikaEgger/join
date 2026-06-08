8a9dffd7cab96072adf95638495968a8e7ca9c33
Annika Egger <annikaegger83@gmail.com>
2026-06-08 15:53:15 +0200

    fix: cards are now rendered back into their old column in a normal, straight position + lowering timeout for dragging to 50ms instead of 200ms so its more intuitive to use

--- scripts/board/board-drag-card.js
+++ scripts/board/board-drag-card.js
@@ -34,7 +34,7 @@ function initMobileTouch(event, card) {
     isLongPress = true; //
     activateMobileDragStyle(card);
     if (navigator.vibrate) navigator.vibrate(50);
-  }, 200);
+  }, 50);
 }
 
 /**
@@ -171,6 +171,8 @@ function stopDragging(event) {
     let columnContainer = element ? element.closest(".card-container") : null;
     if (columnContainer) moveTo(columnContainer.id);
   }
+
+  updateHTML();
 }
 
 /**
